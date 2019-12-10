bad0d1f8effa61ece2a9444c8b4f42602363d681
Flavien Oulhiou-Naudin <flavien.naudin@gmail.com>
2019-12-10 22:01:22 +0100

    v1.6_TP3

--- src/main/java/TestSelect.java
+++ src/main/java/TestSelect.java
@@ -4,7 +4,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.ResourceBundle;
 
 import fr.diginamic.jdbc.entites.Fournisseur;
